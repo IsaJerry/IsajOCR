e2d913cdcb31d93ed9c81d4ade61d3204392c6bd
IsaJerry <seasonjerry154@gmail.com>
2024-08-09 16:11:00 +0800

    v0.0.0 Alpha

---
+++ untitled/ISAJ.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import net.miginfocom.swing.*;
+/*
+ * Created by JFormDesigner on Mon Jan 08 17:00:47 CST 2024
+ */
+
+
+
+/**
+ * @author IsaJerry
+ */
+public class ISAJ extends JFrame {
+    public ISAJ() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new MigLayout(
+            "hidemode 3",
+            // columns
+            "[fill]" +
+            "[fill]",
+            // rows
+            "[]" +
+            "[]" +
+            "[]"));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
